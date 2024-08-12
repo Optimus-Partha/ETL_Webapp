db6bf17ea421d669fa25e08edb9ba435db5b3c19
Partha Singha <51717029+Optimus-Partha@users.noreply.github.com>
2024-08-12 14:28:36 +0530

    Update app.py
    port update

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, render_template, send_file
 import pandas as pd
 import io
+import os
 
 app = Flask(__name__)
 
@@ -36,4 +37,5 @@ def process_file():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
